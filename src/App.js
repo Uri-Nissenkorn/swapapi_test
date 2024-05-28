61f4bf6cc9808093a51f3eab9fb9b73c4178734e
UriNissenkon <urin@mail.tau.ac.il>
2024-05-28 15:37:50 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import { Box } from "@mui/material";
+import { useEffect } from "react";
 
 function App() {
+  const [films, setFilms] = useState([]);
+  const fetchData = async () => {
+    const response = await fetch("https://swapi.dev/api/films/");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+
+    setFilms(data.results);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      {films.map((i) => (
+        <Card key={i}>
+          <CardContent>
+            <h1>{i.title}</h1>
+            <p>{i.release_date}</p>
+            <p>{i.opening_crawl}</p>
+            <p>{i.director}</p>
+            <p>{i.producer}</p>
+            <p>{i.url}</p>
+            <p>{i.episode_id}</p>
+            <p>{i.created}</p>
+            <p>{i.edited}</p>
+          </CardContent>
+        </Card>
+      ))}
+    </Box>
   );
 }
 
